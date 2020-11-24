cd897ef49160651e9a4976756bfc6cc240d0d117
Zulf Ahmed <zulf@pop-os.localdomain>
2020-11-24 01:10:12 -0600

    Running into Meteor specific issues in SurveyCollection.insert.

--- imports/api/SurveyCollection.js
+++ imports/api/SurveyCollection.js
@@ -1,10 +1,53 @@
 import { Mongo } from 'meteor/mongo';
 const JSON = require('dirty-json');
+const fs = require('fs');
 
+function json_from_survey_text_file(file_path, qn){
+    var data = fs.readFileSync( file_path, 'utf8' );
+    var lines = data.split("\n");
+    var n = lines.length;
+    var j = 0;
+    var out = "\{ \"name\" : \"" + qn + "\",  \"value\" :[ ";
+    lines.forEach( (line) => {
+	if (line) {
+	    var v = line.split(':')
+	    var w = String(v[1]).replace("\"",""); // kill the quotes
+	    var inc_out  = " \{ \"question\" : \"" + w + "\" ,";
+	    inc_out = inc_out + " \"answers\" : [";
+	    inc_out = inc_out + "  \{ \"type\" : \"1\", \"content\": \"1\" \},";
+	    inc_out = inc_out + "  \{ \"type\" : \"2\", \"content\": \"2\" \},";
+	    inc_out = inc_out + "  \{ \"type\" : \"3\", \"content\": \"3\" \},";
+	    inc_out = inc_out + "  \{ \"type\" : \"4\", \"content\": \"4\" \},";
+	    inc_out = inc_out + "  \{ \"type\" : \"5\", \"content\": \"5\" \} ] \} ";
+	    j = j + 1;
+	    if (j < n-1){
+		inc_out = inc_out + ',';
+	    }
+	    out = out + inc_out;
+	}
+    });
+    out = out + " ] \}";
+    return(out);
+}
+
+// SurveyCollection is a specialization of Collection
+// that has specific knowledge of what to do with
+// file contents.  We don't want to spread this knowledge
+// outside this class.
 
 class SurveyCollection extends Mongo.Collection {
-    insert( json_obj ){
-	return Mongo.Collection.prototype.insert( json_obj );
+
+    insert( files ){
+	//console.log(fields);
+	console.log(files);
+	var file_path = files['myFile']['path'];
+	var file_name = files['myFile']['name'];
+	console.log(file_path);
+	console.log(file_name);
+	var json_file_text = json_from_survey_text_file(
+	    file_path, file_name);
+	var json_obj = JSON.parse(json_file_text);
+	return super.insert( json_obj );
     }
 };
 
--- server/main.js
+++ server/main.js
@@ -16,50 +16,16 @@ Meteor.startup(() => {
     }
 });
 
-function json_from_survey_text_file(file_path, qn){
-    var data = fs.readFileSync( file_path, 'utf8' );
-    var lines = data.split("\n");
-    var n = lines.length;
-    var j = 0;
-    var out = "\{ \"name\" : \"" + qn + "\",  \"value\" :[ ";
-    lines.forEach( (line) => {
-	if (line) {
-	    var v = line.split(':')
-	    var w = String(v[1]).replace("\"",""); // kill the quotes
-	    var inc_out  = " \{ \"question\" : \"" + w + "\" ,";
-	    inc_out = inc_out + " \"answers\" : [";
-	    inc_out = inc_out + "  \{ \"type\" : \"1\", \"content\": \"1\" \},";
-	    inc_out = inc_out + "  \{ \"type\" : \"2\", \"content\": \"2\" \},";
-	    inc_out = inc_out + "  \{ \"type\" : \"3\", \"content\": \"3\" \},";
-	    inc_out = inc_out + "  \{ \"type\" : \"4\", \"content\": \"4\" \},";
-	    inc_out = inc_out + "  \{ \"type\" : \"5\", \"content\": \"5\" \} ] \} ";
-	    j = j + 1;
-	    if (j < n-1){
-		inc_out = inc_out + ',';
-	    }
-	    out = out + inc_out;
-	}
-    });
-    out = out + " ] \}";
-    return(out);
-}
-
 var postRoutes = Picker.filter(function(req, res) {
    // here you could be a bit more specific in which
    // post request you are looking for, e.g. check the req.url
    if (req.method == "POST") {
        const form = new formidable.IncomingForm(); 
        form.parse(req, function(err, fields, files){
-	   console.log(fields);
-	   console.log(files);
-	   var file_path = files['myFile']['path'];
-	   var file_name = files['myFile']['name'];
-	   console.log(file_path);
-	   console.log(file_name);
-	   var json_file_text = json_from_survey_text_file(
-	       file_path, file_name);
-	   var json_obj = JSON.parse(json_file_text);
-	   Surveys.insert( json_obj );
+	   // We don't want knowledge of Questionnaire
+	   // specific processing. Formidable 'files'
+	   // constains low level file information
+	   Surveys.insert( files );
        });
        
    }
